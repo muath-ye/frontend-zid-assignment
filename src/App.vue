bdc01aabe834f0c3cb279b02d24275bbbfc2019e
muathye <muath.ye@gmail.com>
2022-10-08 09:28:35 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-
+import Sidebar from "./components/Sidebar.vue"
 </script>
 
 <template>
+  <div class="app">
+  <Sidebar />
+
   <router-view />
+  </div>
 </template>
-
-<style lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
