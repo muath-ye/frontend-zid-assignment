a5d8dc286074014b74b87f14dcb2175251cf35b5
muathye <muath.ye@gmail.com>
2022-10-08 20:10:54 +0300

    Add profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,27 @@
 <template>
     <main id="home-page">
-        <h1>Home</h1>
-        <p>This is the home page</p>
+        <div class="w-100 rounded-lg overflow-hidden self-center grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+            <div class="bg-white dark:bg-zinc-800 mt-10 rounded-lg">
+                <div class="flex items-center justify-center pt-10 flex-col">
+                    <img src="https://avatars.githubusercontent.com/u/71807824?s=200&v=4" class="rounded-full w-32">
+                    <h1 class="text-gray-800 dark:text-white font-semibold text-xl mt-5">Muath Alsowadi</h1>
+                    <h1 class="text-gray-500 text-sm dark:text-white-700">Senior Developer</h1>
+                    <h1 class="text-gray-500 dark:text-gray-100 text-sm p-4 text-center">
+                        Fan of @laravel ❯ Co-Founder of @Yemeni-Open-Source ❯ Ready for
+                        state-of-the-art projects.
+                    </h1>
+                </div>
+                <div class="flex justify-around p-4">
+                    <div>
+                        <h1 class="text-xs uppercase text-gray-500 ark:text-white-700">Gender</h1>
+                        <h1 class="text-xs text-yellow-500">Male</h1>
+                    </div>
+                    <div>
+                        <h1 class="text-xs uppercase text-gray-500 ark:text-white-700">DOB</h1>
+                        <h1 class="text-xs text-green-500">1994</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
