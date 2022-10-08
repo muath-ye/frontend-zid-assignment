339e026b3ef4709bffc4366783d37e437a6804b1
muathye <muath.ye@gmail.com>
2022-10-08 12:11:29 +0300

    Update sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -36,70 +36,129 @@ const ToggleMenu = () => {
 
 <style lang="scss" scoped>
 aside {
+  display: flex;
+  flex-direction: column;
+  width: calc(2rem);
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 1rem;
+  transition: 0.4s ease-out;
+  box-shadow:
+    0 8px 10px -5px rgb(0 0 0 / 20%),
+    0 16px 24px 2px rgb(0 0 0 / 14%),
+    0 6px 30px 5px rgb(0 0 0 / 12%);
+  .logo {
+    margin-bottom: 1rem;
+    img {
+      width: 2rem;
+    }
+  }
+  .menu-toggle-wrap {
     display: flex;
-    flex-direction: column;
-	width: calc(2rem);
-    min-height: 100vh;
-    overflow: hidden;
-    padding: 1rem;
-    transition: 0.4s ease-out;
-    box-shadow:
-        0 8px 10px -5px rgb(0 0 0 / 20%),
-        0 16px 24px 2px rgb(0 0 0 / 14%),
-        0 6px 30px 5px rgb(0 0 0 / 12%);
-    .logo {
-        margin-bottom: 1rem;
-        img {
-            width: 2rem;
+    justify-content: flex-end;
+    margin-bottom: 1rem;
+    position: relative;
+    top: 0;
+    transition: 0.4s ease-in-out;
+
+    .menu-toggle {
+      transition: 0.4s ease-out;
+
+      .material-icons {
+        font-size: 2rem;
+        color: var(--light);
+        transition: 0.4s ease-out;
+      }
+
+      &:hover {
+        .material-icons {
+        color: var(--primary);
+        transform: translateX(0.5rem);
         }
+      }
     }
-    .menu-toggle-wrap {
+  }
+
+  h3, .button, .text {
+      opacity: 0;
+      transition: 0.5s ease-out;
+  }
+
+  h3 {
+    color: var(--dark-alt);
+    font-size: 0.875rem;
+    margin-bottom: 0.5rem;
+    text-transform: uppercase;
+    cursor: default;
+  }
+  .menu {
+      margin: 0 -1rem;
+
+      .button {
         display: flex;
-        justify-content: flex-end;
-        margin-bottom: 1rem;
-        position: relative;
-        top: 0;
-        transition: 0.4s ease-in-out;
+        align-items: center;
+        text-decoration: none;
+        padding: 0.5rem 1rem;
+        transition: 0.2s ease-out;
+        opacity: 1;
 
-        .menu-toggle {
-            transition: 0.4s ease-out;
+        .material-icons {
+          font-size: 2rem;
+          color: var(--light);
+          transition: 0.2s ease-out;
+        }
 
-            .material-icons {
-                font-size: 2rem;
-                color: var(--light);
-                transition: 0.4s ease-out;
-            }
+        .text {
+          color: var(--light);
+          transition: 0.2s ease-out;
+        }
 
-            &:hover {
-                .material-icons {
-                    color: var(--primary);
-                    transform: translateX(0.5rem);
-                }
-            }
+        &:hover, &.router-link-exact-active {
+          background-color: var(--dark-alt);
+
+          .material-icons, .text {
+            color: var(--primary);
+          }
         }
-    }
-    &.is-expanded {
-        width: var(--expanded-sidebar-width);
-        .menu-toggle-wrap {
-            top: -3rem;
-            .menu-toggle {
-            transform: rotate(-180deg);
-            .material-icons {
-                color: var(--light);
-            }
-            &:hover {
-                .material-icons {
-                    color: var(--primary);
-                    transform: translateX(0.5rem);
-                }
-            }
+      }
+      .router-link-exact-active {
+        border-left: 0.2rem;
+        border-left-color: var(--primary);
+        border-left-style: solid;
+      }
+  }
+
+  &.is-expanded {
+    width: var(--expanded-sidebar-width);
+    .menu-toggle-wrap {
+      top: -3rem;
+      .menu-toggle {
+        transform: rotate(-180deg);
+        .material-icons {
+          color: var(--light);
         }
+        &:hover {
+          .material-icons {
+            color: var(--primary);
+            transform: translateX(0.5rem);
+          }
         }
+      }
     }
-    @media (max-width: 768px) {
-        position: fixed;
-        z-index: 99;
+    h3, .button, .text {
+      opacity: 1;
     }
+
+    .button {
+      .material-icons {
+        margin-right: 1rem;
+      }
+    }
+  }
+  @media (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+  }
 }
 @media (prefers-color-scheme: dark) {
   aside {
