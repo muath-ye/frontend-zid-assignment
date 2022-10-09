001f13849c0f4b9f3d7bc94371250eadee43baa9
muathye <muath.ye@gmail.com>
2022-10-09 21:49:55 +0300

    Add laravel valet to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs';
+
+const host = 'credit-card-challenge.test';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: { // [!tl add]
+    host, // [!tl add]
+    hmr: { host }, // [!tl add]
+    https: { // [!tl add]
+        key: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.key`), // [!tl add]
+        cert: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.crt`), // [!tl add]
+    }, // [!tl add]
+}, // [!tl add]
 })
