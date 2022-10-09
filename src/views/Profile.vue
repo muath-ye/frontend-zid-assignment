97fe75c3151b6f0c16b7205b786033dcafaf3f47
muathye <muath.ye@gmail.com>
2022-10-09 23:25:19 +0300

    Update profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,25 +2,25 @@
     <main id="home-page">
         <div
             class="w-100 rounded-lg overflow-hidden self-center lg:flex lg:justify-center grid lg:grid-cols-1 md:grid-cols-2 grid-cols-1 gap-4 bg-white dark:bg-zinc-800 mt-10 rounded-lg">
-            <img src="https://pbs.twimg.com/profile_images/1579203775868420097/sp45IOuf_400x400.jpg"
-                class="rounded-full xl:w-96 w-28 lg:w-96 md:w-56 sm:w-44 sm:mx-auto lg:mx-px mx-auto m-3">
+            <img src="https://pbs.twimg.com/profile_images/1579204370205384713/sIe7zTS8_400x400.jpg"
+                class="rounded-full xl:w-96 w-28 lg:w-96 md:w-56 sm:w-44 sm:mx-auto lg:mx-px mx-auto m-3 hover:shadow-zinc-900">
 
-            <div class="flex items-center justify-center md:pt-10 sm:pt-0 flex-col">
-                <h1 class="text-gray-800 dark:text-white font-semibold text-xl mt-5">Muath Alsowadi</h1>
+            <div class="flex items-center justify-center flex-col">
+                <h1 class="text-gray-800 dark:text-white font-semibold text-xl mt-5 hover:text-green-500">Muath Alsowadi</h1>
                 <h1 class="text-gray-500 text-sm dark:text-white-700">Senior Developer</h1>
                 <h1 class="text-gray-500 dark:text-gray-100 text-sm p-4 text-center">
                     Fan of @laravel ❯ Co-Founder of @Yemeni-Open-Source ❯ Ready for
                     state-of-the-art projects.
                 </h1>
 
-                <div class="flex p-16">
+                <div class="flex">
                     <div class="mx-3">
-                        <h1 class="text-xs uppercase text-gray-500 ark:text-white-700">Gender</h1>
-                        <h1 class="text-xs text-yellow-500">Male</h1>
+                        <h1 class="text-xs text-green-500 uppercase ark:text-white-700">Gender</h1>
+                        <h1 class="text-xs text-gray-500">Male</h1>
                     </div>
                     <div class="mx-3">
-                        <h1 class="text-xs uppercase text-gray-500 ark:text-white-700">DOB</h1>
-                        <h1 class="text-xs text-green-500">1994</h1>
+                        <h1 class="text-xs text-green-500 uppercase ark:text-white-700">DOB</h1>
+                        <h1 class="text-xs text-gray-500">1994</h1>
                     </div>
                 </div>
             </div>
