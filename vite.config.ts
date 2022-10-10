1bc0aee987752a395b8d0f24ba3d775399dc4e08
muathye <muath.ye@gmail.com>
2022-10-10 08:21:00 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ const host = 'credit-card-challenge.test';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: { // [!tl add]
-    host, // [!tl add]
-    hmr: { host }, // [!tl add]
-    https: { // [!tl add]
-        key: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.key`), // [!tl add]
-        cert: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.crt`), // [!tl add]
-    }, // [!tl add]
-}, // [!tl add]
+  // server: { // [!tl add]
+  //   host, // [!tl add]
+  //   hmr: { host }, // [!tl add]
+  //   https: { // [!tl add]
+  //     key: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.key`), // [!tl add]
+  //     cert: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.crt`), // [!tl add]
+  //   }, // [!tl add]
+  // }, // [!tl add]
 })
