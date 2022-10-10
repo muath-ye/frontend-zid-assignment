0d9206344417b6187f70e3a0eae1ec331b27c74d
muathye <muath.ye@gmail.com>
2022-10-10 21:13:32 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs';
 
+const hash = Math.floor(Math.random() * 90000) + 10000;
 // const host = 'credit-card-challenge.test';
 
 // https://vitejs.dev/config/
@@ -15,4 +16,13 @@ export default defineConfig({
   //     cert: fs.readFileSync(`C:/Users/Administrator/.config/valet/Certificates/${host}.crt`), // [!tl add]
   //   }, // [!tl add]
   // }, // [!tl add]
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `asset/[name]` + hash + `.js`,
+        chunkFileNames: `asset/[name]` + hash + `.js`,
+        assetFileNames: `asset/[name]` + hash + `.[ext]`
+      }
+    }
+  }
 })
